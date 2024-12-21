419921d9191484ce54c96b582fe112cd60840d89
Ranjan Giri <giriranjan249@gmail.com>
2024-12-21 18:13:36 +0530

    PDDEV-001-project1

---
+++ Practise_Project/demo/demo/src/main/java/com/example/demo/JobController.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+
+
+import com.example.demo.model.JobPost;
+import com.example.demo.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class JobController {
+
+    @Autowired
+    private JobService service;
+    @GetMapping({"/","home"})
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("addjob")
+    public String addJob(){
+        return "addjob";
+    }
+
+    @PostMapping("handleForm")
+    public String handleForm(JobPost jobPost){
+        service.addJobPost(jobPost);
+        return "success";
+    }
+    @GetMapping("/viewalljobs")
+    public String viewJobs(Model model) {
+
+        List<JobPost> jobPosts = service.returnAllJobPosts();
+        model.addAttribute("jobPosts", jobPosts);
+        return "viewalljobs";
+    }
+
+}
